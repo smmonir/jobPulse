241c8bc2d2ea90c13bbc2f4e445c60e64cb370ef
Monir Uddin <eng.smmonir@gmail.com>
2024-03-15 22:18:04 +0600

    second commit

--- database/migrations/2024_03_12_054503_create_candidate_profiles_table.php
+++ database/migrations/2024_03_12_054503_create_candidate_profiles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('company_profile', function (Blueprint $table) {
+        Schema::create('candidate_profiles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('company_profile');
+        Schema::dropIfExists('candidate_profiles');
     }
 };
--- database/migrations/2024_03_12_054540_create_company_profiles_table.php
+++ database/migrations/2024_03_12_054540_create_company_profiles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('candidate_profile', function (Blueprint $table) {
+        Schema::create('company_profiles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('candidate_profile');
+        Schema::dropIfExists('company_profiles');
     }
 };
